f8e19aa30abde0924bf9a46b798765f7b6c6bcc2
iprohorov <iprohorov@mmtr.ru>
2023-07-25 17:23:04 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObjectCardComponent } from './components/object-card/object-card.component';
+import {Swiper} from "./components/swiper/swiper.component";
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObjectCardComponent,
+    Swiper,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
